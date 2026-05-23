d3cdb75b1afde999036cc869a3ac1537293e6c9e
samuelreichor <samuelreichor@gmail.com>
2026-05-23 16:09:05 +0200

    Add translations for de users

---
+++ translations/de/site.php
@@ -0,0 +1,73 @@
+<?php
+return [
+    // Sections / Element-Source-Pages
+    'Home' => 'Startseite',
+    'Pages' => 'Seiten',
+    'Contact' => 'Kontakt',
+    'Error Pages' => 'Fehlerseiten',
+    'Globals' => 'Globale Inhalte',
+
+    // Entry Types
+    'Pagebuilder' => 'Seitenbaukasten',
+    'Image and Text' => 'Bild und Text',
+    'Icon with Text' => 'Icon mit Text',
+    'Icon with Columns' => 'Icon mit Spalten',
+    'Anchor' => 'Anker',
+    'Quote' => 'Zitat',
+    'Calendly' => 'Calendly',
+
+    // Felder
+    'Address' => 'Adresse',
+    'Alignment' => 'Ausrichtung',
+    'Alt Text' => 'Alternativtext',
+    'Icon' => 'Icon',
+    'Caption' => 'Bildunterschrift',
+    'Columns with Icons' => 'Spalten mit Icons',
+    'Content' => 'Inhalt',
+    'E-Mail' => 'E-Mail',
+    'Headline' => 'Überschrift',
+    'Headline Tag' => 'Überschrift-Tag',
+    'Hero' => 'Hero',
+    'Hero Contact' => 'Hero Kontakt',
+    'Image' => 'Bild',
+    'Image Ratio' => 'Bildverhältnis',
+    'Light Switch' => 'Schalter',
+    'Link' => 'Link',
+    'Navigations Links' => 'Navigationslinks',
+    'Plain Text Multiline' => 'Einfacher Text (mehrzeilig)',
+    'Simple Richtext' => 'Einfacher Richtext',
+    'Text' => 'Text',
+    'Telephone' => 'Telefon',
+    'Code' => 'Code',
+
+    // Field-Layout-Tabs
+    'Settings' => 'Einstellungen',
+    'SEO' => 'SEO',
+
+    // Field-Layout-Element-Labels / Vorschau
+    'Primary entry page' => 'Primäre Eintragsseite',
+    'Anchor Title' => 'Anker-Titel',
+    'Author' => 'Autor',
+    'Background Image' => 'Hintergrundbild',
+    'Navigations Button' => 'Navigations-Button',
+    'Footer Quick Links' => 'Fußzeile Schnelllinks',
+    'Footer Legal Links' => 'Fußzeile Rechtliche Links',
+    'Footer Legal Text' => 'Fußzeile Rechtlicher Text',
+    'Switched' => 'Vertauscht',
+    'Switch image and text column order' => 'Reihenfolge von Bild- und Textspalte tauschen',
+
+    // Volumes
+    'Files' => 'Dateien',
+    'Images' => 'Bilder',
+    'Template Images' => 'Template-Bilder',
+
+    // User-Gruppen
+    'Editors' => 'Redakteure',
+
+    // Dropdown-Optionen
+    'Left' => 'Links',
+    'Center' => 'Zentriert',
+    'Landscape' => 'Querformat',
+    'Auto' => 'Automatisch',
+    'Square' => 'Quadratisch',
+];
